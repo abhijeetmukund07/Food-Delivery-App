4e1c0c6e0ec2067f5ce16ba5da0d4631ccfaddd2
abhijeetmukund07 <abhijeetmukund@gmail.com>
2024-04-28 12:48:37 +0530

    1. Signin and SignOut button appears on the basis of loginStatus.
    2. SignOut button deletes the token from session storage and redirects to login page in case of restaurant and home page in case of user.

--- frontend/src/ClientComponents/ClientNavbar/ClientNavbar.jsx
+++ frontend/src/ClientComponents/ClientNavbar/ClientNavbar.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import './ClientNavbar.css'
 import {assets} from '../../assets/admin_assets/assets'
+import { NavLink } from "react-router-dom";
+import { useSelector,useDispatch } from "react-redux";
+import { resetState } from "../../redux/userLoginSlice";
 function ClientNavbar() {
+
+  const {loginStatus,currentUser} = useSelector(state=>state.userLogin)
+  const dispatch = useDispatch()
+  function logOut(){
+    //remove token from session storage
+    sessionStorage.removeItem('token')
+    //reset the state
+    let resetActionObj = resetState()
+    //dispatch the resetActionObj to redux store to reset the userLogin State
+    dispatch(resetActionObj)
+  }
+
   return (
     <div className="client-navbar">
         <img src={assets.logo} alt="logo" className="logo" />
-        <img src={assets.profile_image} alt="profile-image" className="profile-img" />
+
+        <div className="custom-client-navbar-right">
+          <img src={assets.profile_image} alt="profile-image" className="profile-img" />
+          {loginStatus&&
+          <NavLink to="/login">
+              <button className="custom-client-navbar-btn" onClick={logOut}>Sign Out</button>
+          </NavLink>}
+        </div>
     </div>);
 }
 
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,50 +2,80 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { assets } from "../../assets/frontend_assets/assets";
 import { NavLink } from "react-router-dom";
-const Navbar = () => {
+import { resetState } from "../../redux/userLoginSlice";
+import { useSelector, useDispatch } from "react-redux";
 
-const [active, setActive] = useState('home')
+const Navbar = () => {
+  const { loginStatus, currentUser } = useSelector((state) => state.userLogin);
+  const dispatch = useDispatch();
+  function logOut() {
+    //remove token from session storage
+    sessionStorage.removeItem("token");
+    //reset the state
+    let resetActionObj = resetState();
+    //dispatch the resetActionObj to redux store to reset the userLogin State
+    dispatch(resetActionObj);
+  }
+
+  const [active, setActive] = useState("home");
 
   return (
-  <div className="container w-100">
-    <div className="navbar navbar-expand-sm navbar-custom">
-      <NavLink to="/">
-        <img src={assets.logo} alt="Logo" className="logo" />
-      </NavLink>
+    <div className="container w-100">
+      <div className="navbar navbar-expand-sm navbar-custom">
+        <NavLink to="/">
+          <img src={assets.logo} alt="Logo" className="logo" />
+        </NavLink>
 
-      <ul className="nav custom-navbar-menu">
-        <li onClick={()=>setActive('home')} className={active==="home"?"nav-item  custom-navbar-link-active":"nav-item"} >
-          <NavLink id="navlink-home">
-            Home
-          </NavLink>
-        </li>
+        <ul className="nav custom-navbar-menu">
+          <li
+            onClick={() => setActive("home")}
+            className={active === "home" ? "nav-item  custom-navbar-link-active" : "nav-item"}
+          >
+            <NavLink id="navlink-home">Home</NavLink>
+          </li>
 
-        <li onClick={()=>setActive('menu')} className={active==="menu"?"nav-item custom-navbar-link-active":"nav-item"}>
-          <NavLink className="" id="navlink-menu">
-            Menu
-          </NavLink>
-        </li>
+          <li
+            onClick={() => setActive("menu")}
+            className={active === "menu" ? "nav-item custom-navbar-link-active" : "nav-item"}
+          >
+            <NavLink className="" id="navlink-menu">
+              Menu
+            </NavLink>
+          </li>
 
-        <li onClick={()=>setActive('contactUs')} className={active==="contactUs"?"nav-item  custom-navbar-link-active":"nav-item"}>
-          <NavLink className="" id="navlink-contact-us">
-            Contact Us
-          </NavLink>
-        </li>
-      </ul>
+          <li
+            onClick={() => setActive("contactUs")}
+            className={active === "contactUs" ? "nav-item  custom-navbar-link-active" : "nav-item"}
+          >
+            <NavLink className="" id="navlink-contact-us">
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
 
-      <div className="custom-navbar-right">
+        <div className="custom-navbar-right">
           <img src={assets.search_icon} alt="" />
 
           <div className="custom-navbar-basket-icon">
             <img src={assets.basket_icon} alt="" />
             <div className="custom-navbar-cart-indicator"></div>
           </div>
-          <NavLink to="/login">
-            <button className=" custom-navbar-btn">sign in</button>
-          </NavLink>
+
+          {loginStatus === false ? (
+            <NavLink to="/login">
+              <button className=" custom-navbar-btn">Sign in</button>
+            </NavLink>
+          ) : (
+            <NavLink to="/">
+              <button className=" custom-navbar-btn" onClick={logOut}>
+                Sign Out
+              </button>
+            </NavLink>
+          )}
+        </div>
       </div>
     </div>
-  </div>)
+  );
 };
 
 export default Navbar;
